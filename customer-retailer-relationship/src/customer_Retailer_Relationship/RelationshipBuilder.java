8002edecc511b7268ca210a2dd7e1f728d0ee505
Fabian Kübler <fabian.kuebler@zweitag.de>
2015-11-25 13:39:54 +0100

    add initial data

--- customer-retailer-relationship/src/customer_Retailer_Relationship/RelationshipBuilder.java
+++ customer-retailer-relationship/src/customer_Retailer_Relationship/RelationshipBuilder.java
@@ -1,5 +1,8 @@
 package customer_Retailer_Relationship;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import repast.simphony.context.Context;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.engine.environment.RunEnvironment;
@@ -8,9 +11,17 @@ public class RelationshipBuilder implements ContextBuilder<Object> {
 	
 	@Override
 	public Context<Object> build(Context<Object> context) {
+		
+		List<Echelon[]> echelons = new ArrayList<Echelon[]>();
+		List<Retailer> echelonList["retailer"] = new ArrayList<Retailer>();
+		
+		
 		context.setId("Customer Retailer Relationship");
 		Customer fabian = new Customer();
 		Retailer jakob = new Retailer();
+		
+		echelonList["retailer"].add(jakob);
+		
 		fabian.setRetailer(jakob);
 		
 		context.add(fabian);
