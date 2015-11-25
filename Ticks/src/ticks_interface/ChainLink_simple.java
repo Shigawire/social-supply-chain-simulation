61d10e5af07693933b564ed7db19c593bff3caf4
Jakob Poggemann <jakob.pogg001@gmail.com>
2015-11-25 13:31:08 +0100

    Revert "Push die ganze scheiße"
    This reverts commit 77418c0520fac5abf7563f5df01fafb677aa5401.

--- Ticks/src/ticks_interface/ChainLink_simple.java
+++
@@ -1,7 +0,0 @@
-package ticks_interface;
-//interface for the simpliest chain link(e.g. customer)s
-public interface ChainLink_simple {
-	public double getDemand();
-	public void setDemand(double newDemand);
-	public void receiveShipment(double shipment);
-}
--- Ticks/src/ticks_interface/InventoryAgent.java
+++
@@ -1,8 +0,0 @@
-package ticks_interface;
-
-public interface InventoryAgent {
-	//method for checking Inventory
-	public void store(double shipment);
-	public void remove(double shipment);
-	public double checkInventory(double customerDemand);
-}
